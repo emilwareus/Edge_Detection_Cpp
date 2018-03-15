23bd0d9b57e3cca9541dabd94d246a615cc85858
Joe Yang <jy22yang@uwaterloo.ca>
2018-03-15 12:00:15 +0800

    Fix the conv function

--- Edge_detection/convolution.cpp
+++ Edge_detection/convolution.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 
 
-std::vector<std::vector<float> > convolution(std::vector<std::vector<uint8_t> >* image, std::vector<std::vector<uint8_t> >* mask) {
-	int n_rows = image->size(); /* rows */
+std::vector<std::vector<float> > convolution(std::vector<std::vector<uint8_t> > &image, std::vector<std::vector<uint8_t> > &mask) {
+	int n_rows = image.size(); /* rows */
 	int n_cols = image[0].size(); /* Collumns*/
 
-	int mask_rows = mask->size(); /* Mask Rows */
+	int mask_rows = mask.size(); /* Mask Rows */
 	int mask_cols = mask[0].size(); /* Mask Collumns*/
 	
 	std::vector<std::vector<float> > result;
@@ -16,7 +16,7 @@ std::vector<std::vector<float> > convolution(std::vector<std::vector<uint8_t> >*
 			float sum = 0;
 			for (int m_row = 0; m_row < mask_rows; m_row++) {
 				for (int m_col = 0; m_col < mask_cols; m_col++) {
-					sum += ((*mask)[m_row][m_col])*((*image)[row][col]);
+					sum += mask[m_row][m_col]*image[row][col];
 				}
 			}
 			temp.push_back(sum / (mask_cols*mask_cols));
