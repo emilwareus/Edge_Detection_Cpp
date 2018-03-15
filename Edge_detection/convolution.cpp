5ea3fb015ed4cf23f8290382fe998ee57732a88f
Joe Yang <jy22yang@uwaterloo.ca>
2018-03-15 12:32:51 +0800

    update convolution method

--- Edge_detection/convolution.cpp
+++ Edge_detection/convolution.cpp
@@ -8,15 +8,17 @@ std::vector<std::vector<float> > convolution(std::vector<std::vector<uint8_t> > 
 
 	int mask_rows = mask.size(); /* Mask Rows */
 	int mask_cols = mask[0].size(); /* Mask Collumns*/
-	
+	int half_mask_rows = mask_rows / 2;	
+	int half_mask_cols = mask_cols / 2;	
+		
 	std::vector<std::vector<float> > result;
 	for (int row = 1; row < n_rows - mask_rows / 2; row++) {
 		std::vector<float> temp;
 		for (int col = 1; col < n_cols - mask_cols / 2; col++) {
 			float sum = 0;
-			for (int m_row = 0; m_row < mask_rows; m_row++) {
-				for (int m_col = 0; m_col < mask_cols; m_col++) {
-					sum += mask[m_row][m_col]*image[row][col];
+			for (int m_row = -half_mask_rows; m_row <= half_mask_rows; m_row++) {
+				for (int m_col = -half_mask_cols; m_col <= half_mask_cols ; m_col++) {
+					sum += mask[half_mask_rows + m_row][half_mask_cols + m_col]*image[row+ m_row][col + m_col];
 				}
 			}
 			temp.push_back(sum / (mask_cols*mask_cols));
--- Edge_detection/convolution.h
+++ Edge_detection/convolution.h
@@ -1,3 +1,3 @@
 #include "convolution.cpp"
 
-std::vector<std::vector<float> > convolution(std::vector<std::vector<uint8_t> >* image, std::vector<std::vector<uint8_t> >* mask);
+std::vector<std::vector<float> > convolution(std::vector<std::vector<uint8_t> >& image, std::vector<std::vector<uint8_t> >& mask);
