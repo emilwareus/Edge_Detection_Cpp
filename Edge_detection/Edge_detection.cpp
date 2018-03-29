cd059f4b5f8c5e769debcd70f3ce134b1e936a5b
Emil Wåreus <emil.wareus47@gmail.com>
2018-03-29 10:59:15 +0800

    some more work done

--- Edge_detection/Edge_detection.cpp
+++ Edge_detection/Edge_detection.cpp
@@ -23,6 +23,7 @@ int rows;
 unsigned int bitNum;
 
 
+
 float PI = 3.14;
 
 int** gaussianMask(int std) {	
@@ -104,6 +105,314 @@ void print3x3Mask(float** mask){
 		cout << endl;
 	}
 }
+
+
+
+void edge_detection(int **image, int image_rows, int image_cols, int **gaussianFilter, const int mask_rows, const int mask_cols, int **sobelMask_x, int **sobelMask_y, int sobelmask_rows, int sobelmask_cols, int gaussian_normfactor, bool save) {
+	
+
+
+
+	//Output 2d-array
+	int **blur_image;
+
+	//Perform GaussianBlur
+	blur_image = convolution_norm(image, image_rows, image_cols, gaussianFilter, mask_rows, mask_cols, gaussian_normfactor);
+
+
+
+	//Scale our output image
+	int blur_rows = image_rows - mask_rows + 1;
+	int blur_cols = image_cols - mask_cols + 1;
+
+
+	int **xmask_image;
+	int **ymask_image;
+
+	//Perform Edge detection
+
+	xmask_image = convolution(blur_image, blur_rows, blur_cols, sobelMask_x, sobelmask_rows, sobelmask_cols);
+	ymask_image = convolution(blur_image, blur_rows, blur_cols, sobelMask_y, sobelmask_rows, sobelmask_cols);
+
+
+
+
+	//Rescale image
+	int out_rows = blur_rows - sobelmask_rows + 1;
+	int out_cols = blur_cols - sobelmask_cols + 1;
+
+	//Output 2d-array
+	int **sobel_image;
+	sobel_image = new int *[out_rows];
+
+	int **edges_image;
+	edges_image = new int *[out_rows];
+
+	//edge magnitude and direction
+	for (int i = 0; i<out_rows; i++) {
+		sobel_image[i] = new int[out_cols];
+		edges_image[i] = new int[out_cols];
+		for (int j = 0; j<out_cols; j++) {
+			sobel_image[i][j] = int(sqrt(xmask_image[i][j] * xmask_image[i][j] + ymask_image[i][j] * ymask_image[i][j]));
+
+
+			if (sobel_image[i][j]>255) {
+				sobel_image[i][j] = 255;
+			}
+			else if (sobel_image[i][j]<0) {
+				sobel_image[i][j] = 0;
+			}
+
+
+			float temp_dir = atan2(ymask_image[i][j], xmask_image[i][j]) / PI * 180;
+			if ((temp_dir>-22.5 && temp_dir <= 22.5) || (temp_dir>-157.5 && temp_dir <= 157.5)) {
+				temp_dir = 0;
+			}
+			else if ((temp_dir>22.5 && temp_dir <= 67.5) || (temp_dir>112.5 && temp_dir <= -157.5)) {
+				temp_dir = 45;
+			}
+			else if ((temp_dir>67.5 && temp_dir <= 112.5) || (temp_dir <= -67.5 && temp_dir >= -112.5)) {
+				temp_dir = 90;
+			}
+			else if ((temp_dir>112.5 && temp_dir <= 157.5) || (temp_dir>-22.5 && temp_dir <= -67.5)) {
+				temp_dir = 135;
+			}
+			edges_image[i][j] = temp_dir;
+		}
+	}
+
+	//Non maximum suppression
+	int ** thinned_edges_image;
+	thinned_edges_image = new int *[out_rows];
+
+	for (int i = 0; i<out_rows; i++) {
+		thinned_edges_image[i] = new int[out_cols];
+		for (int j = 0; j<out_cols; j++) {
+			thinned_edges_image[i][j] = sobel_image[i][j];
+
+		}
+	}
+
+	for (int i = 1; i<out_rows - 1; i++) {
+		for (int j = 1; j<out_cols - 1; j++) {
+			if (edges_image[i][j] == 0) {
+				if ((sobel_image[i][j] < sobel_image[i][j + 1]) || (sobel_image[i][j] < sobel_image[i][j - 1])) {
+					thinned_edges_image[i][j] = 0;
+				}
+			}
+			else if (edges_image[i][j] == 90) {
+				if ((sobel_image[i][j] < sobel_image[i + 1][j]) || (sobel_image[i][j] < sobel_image[i - 1][j])) {
+					thinned_edges_image[i][j] = 0;
+				}
+			}
+			else if (edges_image[i][j] == 135) {
+				if ((sobel_image[i][j] < sobel_image[i - 1][j + 1]) || (sobel_image[i][j] < sobel_image[i + 1][j - 1])) {
+					thinned_edges_image[i][j] = 0;
+				}
+			}
+			else if (edges_image[i][j] == 45) {
+				if ((sobel_image[i][j] < sobel_image[i + 1][j + 1]) || (sobel_image[i][j] < sobel_image[i - 1][j - 1])) {
+					thinned_edges_image[i][j] = 0;
+				}
+			}
+
+
+		}
+	}
+
+
+
+
+	//Thresholding
+	int ** threshold_image;
+	threshold_image = new int *[out_rows];
+
+	for (int i = 0; i<out_rows; i++) {
+		threshold_image[i] = new int[out_cols];
+		for (int j = 0; j<out_cols; j++) {
+			if (thinned_edges_image[i][j] < 60) {
+				threshold_image[i][j] = 0;
+			}
+			else if (thinned_edges_image[i][j] > 100) {
+
+				threshold_image[i][j] = 255;
+			}
+			else {
+				threshold_image[i][j] = 127;
+			}
+		}
+	}
+
+
+	//Hysteresis
+	int ** hysteresis_image;
+	hysteresis_image = new int *[out_rows];
+
+
+	for (int i = 1; i<out_rows; i++) {
+		hysteresis_image[i] = new int[out_cols];
+		for (int j = 1; j<out_cols; j++) {
+			hysteresis_image[i][j] = threshold_image[i][j];
+		}
+	}
+	int image_has_changed = 1;
+	while (image_has_changed == 1) {
+		image_has_changed = 0;
+		for (int i = 1; i<out_rows - 1; i++) {
+			for (int j = 1; j<out_cols - 1; j++) {
+				if (hysteresis_image[i][j] == 127) {
+					if (hysteresis_image[i - 1][j - 1] == 255 || hysteresis_image[i - 1][j] == 255 || hysteresis_image[i - 1][j + 1] == 255 || hysteresis_image[i][j - 1] == 255 || hysteresis_image[i][j + 1] == 255 || hysteresis_image[i + 1][j - 1] == 255 || hysteresis_image[i + 1][j] == 255 || hysteresis_image[i + 1][j + 1] == 255) {
+						hysteresis_image[i][j] = 255;
+						image_has_changed = 1;
+					}
+				}
+			}
+		}
+	}
+	for (int i = 1; i<out_rows; i++) {
+		for (int j = 1; j<out_cols; j++) {
+			if (hysteresis_image[i][j] == 127) {
+				hysteresis_image[i][j] = 0;
+			}
+		}
+	}
+
+
+
+	//output CV_matrix for display
+	cv::Mat blur_mat(blur_rows, blur_cols, CV_8U);
+
+	//Add pixels to matrix
+	for (int i = 0; i < blur_rows; ++i) {
+		for (int j = 0; j < blur_cols; ++j) {
+			blur_mat.at<uint8_t>(i, j) = uint8_t(blur_image[i][j]);
+
+
+		}
+	}
+
+	//Display matrix
+	imshow("Blur_image", blur_mat);
+
+	//___________________
+
+	//output CV_matrix for display
+	cv::Mat out_mat(out_rows, out_cols, CV_8UC1);
+
+	//Add pixels to matrix
+	for (int i = 0; i < out_rows; ++i) {
+		for (int j = 0; j < out_cols; ++j) {
+			out_mat.at<uint8_t>(i, j) = uint8_t(sobel_image[i][j]);
+
+
+		}
+	}
+
+
+	//Display matrix
+	imshow("Sobel_image", out_mat);
+
+	//___________________
+
+
+
+	//output CV_matrix for display
+	cv::Mat thinned_edges_mat(out_rows, out_cols, CV_8UC1);
+
+	//Add pixels to matrix
+	for (int i = 0; i < out_rows; ++i) {
+		for (int j = 0; j < out_cols; ++j) {
+			thinned_edges_mat.at<uint8_t>(i, j) = uint8_t(thinned_edges_image[i][j]);
+
+		}
+	}
+
+	//Display matrix
+	imshow("Thinned_edges_image", thinned_edges_mat);
+
+
+	//________________
+
+
+	//output CV_matrix for display
+	cv::Mat threshold_mat(out_rows, out_cols, CV_8UC1);
+
+	//Add pixels to matrix
+	for (int i = 0; i < out_rows; ++i) {
+		for (int j = 0; j < out_cols; ++j) {
+			threshold_mat.at<uint8_t>(i, j) = uint8_t(threshold_image[i][j]);
+
+		}
+	}
+	//Display matrix
+	imshow("Threshold_image", threshold_mat);
+
+
+	//________________
+
+	//output CV_matrix for display
+	cv::Mat hysteresis_mat(out_rows, out_cols, CV_8UC1);
+
+	//Add pixels to matrix
+	for (int i = 1; i < out_rows - 1; ++i) {
+		for (int j = 1; j < out_cols - 1; ++j) {
+			hysteresis_mat.at<uint8_t>(i, j) = uint8_t(hysteresis_image[i][j]);
+
+		}
+	}
+	//Display matrix
+	imshow("Hysteresis_image", hysteresis_mat);
+
+
+	//________________
+
+
+	
+	//Saves the images
+	if(save == true){
+
+		save_image("modified_images/original_image.raw", image, image_rows, image_cols);
+		save_image("modified_images/sobel_image.raw", sobel_image, out_rows, out_cols);
+		save_image("modified_images/thinned_edges_image.raw", thinned_edges_image, out_rows, out_cols);
+		save_image("modified_images/threshold_image.raw", threshold_image, out_rows, out_cols);
+		save_image("modified_images/hysteresis_image.raw", hysteresis_image, out_rows, out_cols);
+
+	}
+
+
+	for (int i = 0; i < blur_rows; i++) {
+		delete blur_image[i];
+	}
+	for (int i = 0; i < image_rows; i++) {
+		delete image[i];
+	}
+
+	for (int i = 0; i < out_rows; i++) {
+		delete sobel_image[i];
+		delete edges_image[i];
+		delete threshold_image[i];
+		delete thinned_edges_image[i];
+	}
+
+	for (int i = 1; i < out_rows - 1; i++) {
+		delete hysteresis_image[i];
+	}
+
+	delete thinned_edges_image;
+	delete hysteresis_image;
+	delete sobel_image;
+	delete edges_image;
+	delete threshold_image;
+	delete blur_image;
+	delete image;
+
+
+
+
+
+}
+
+
 int main() {
 
 	//Open Video stream
@@ -185,323 +494,45 @@ int main() {
 
 
 	//unconditional loop
+	/*
 	while (true) {
 		
-		Mat gray, raw;
+		Mat raw;
 
 		//Grab image from camera
 		stream1.read(raw);
 
+		Mat gray;
 		//Change to gray scale
 		cvtColor(raw, gray, CV_RGB2GRAY);
-		
+
 		//Display image
 		imshow("gray", gray);
 
-		
+
 		//2d-array of pixels
 		int **image;
 		image = new int *[gray.rows];
-		
+
 		//Fill with actual pixel-values for image
 		for (int i = 0; i < gray.rows; ++i) {
 			image[i] = new int[gray.cols];
 			for (int j = 0; j < gray.cols; ++j) {
-				image[i][j] =int(gray.at<uint8_t>(i,j));
-				
-			}
-			
-		}
-
-		
-	
-		//Output 2d-array
-		int **blur_image;
-
-		//Perform GaussianBlur
-		blur_image = convolution_norm(image, gray.rows, gray.cols, gaussianFilter, mask_rows, mask_cols, gaussian_normfactor);
-
-
-
-		//Scale our output image
-		int blur_rows = gray.rows - mask_rows + 1;
-		int blur_cols = gray.cols - mask_cols + 1;
-
-		
-		int **xmask_image;
-		int **ymask_image;
-
-		//Perform Edge detection
-
-		xmask_image = convolution(blur_image, blur_rows, blur_cols, sobelMask_x, sobelmask_rows, sobelmask_cols);
-		ymask_image = convolution(blur_image, blur_rows, blur_cols, sobelMask_y, sobelmask_rows, sobelmask_cols);
-
-
-
-
-		//Rescale image
-		int out_rows = blur_rows - sobelmask_rows + 1;
-		int out_cols = blur_cols - sobelmask_cols + 1;
-
-		//Output 2d-array
-		int **sobel_image;
-		sobel_image = new int *[out_rows];
-
-		int **edges_image;
-		edges_image = new int *[out_rows];
-
-		//edge magnitude and direction
-		for (int i = 0; i<out_rows; i++) {
-			sobel_image[i] = new int[out_cols];
-			edges_image[i] = new int[out_cols];
-			for (int j = 0; j<out_cols; j++) {
-				sobel_image[i][j] = int(sqrt(xmask_image[i][j] * xmask_image[i][j] + ymask_image[i][j] * ymask_image[i][j]));
-				
-				
-				if (sobel_image[i][j]>255) {
-					sobel_image[i][j] = 255;
-				}
-				else if (sobel_image[i][j]<0) {
-					sobel_image[i][j] = 0;
-				}
-		
-
-				float temp_dir = atan2(ymask_image[i][j], xmask_image[i][j]) / PI * 180;
-				if ((temp_dir>-22.5 && temp_dir <= 22.5) || (temp_dir>-157.5 && temp_dir <= 157.5)) {
-					temp_dir = 0;
-				}
-				else if ((temp_dir>22.5 && temp_dir <= 67.5) || (temp_dir>112.5 && temp_dir <= -157.5)) {
-					temp_dir = 45;
-				}
-				else if ((temp_dir>67.5 && temp_dir <= 112.5) || (temp_dir <= -67.5 && temp_dir >= -112.5)) {
-					temp_dir = 90;
-				}
-				else if ((temp_dir>112.5 && temp_dir <= 157.5) || (temp_dir>-22.5 && temp_dir <= -67.5)) {
-					temp_dir = 135;
-				}
-				edges_image[i][j] = temp_dir;
-			}
-		}
-
-		//Non maximum suppression
-		int ** thinned_edges_image;
-		thinned_edges_image = new int *[out_rows];
-
-		for (int i = 0; i<out_rows; i++) {
-			thinned_edges_image[i] = new int[out_cols];
-			for (int j = 0; j<out_cols; j++) {	
-				thinned_edges_image[i][j] = sobel_image[i][j];
-				
-			}
-		}
-
-		for (int i = 1; i<out_rows-1; i++) {
-			for (int j = 1; j<out_cols-1; j++) {
-				if (edges_image[i][j] == 0) {
-					if ((sobel_image[i][j] < sobel_image[i][j + 1]) || (sobel_image[i][j] < sobel_image[i][j - 1])) {
-						thinned_edges_image[i][j] = 0;
-					}
-				}
-				else if (edges_image[i][j] == 90) {
-					if ((sobel_image[i][j] < sobel_image[i + 1][j]) || (sobel_image[i][j] < sobel_image[i - 1][j])) {
-						thinned_edges_image[i][j] = 0;
-					}
-				}
-				else if (edges_image[i][j] == 135) {
-					if ((sobel_image[i][j] < sobel_image[i - 1][j + 1]) || (sobel_image[i][j] < sobel_image[i + 1][j - 1])) {
-						thinned_edges_image[i][j] = 0;
-					}
-				}
-				else if (edges_image[i][j] == 45) {
-					if ((sobel_image[i][j] < sobel_image[i + 1][j + 1]) || (sobel_image[i][j] < sobel_image[i - 1][j - 1])) {
-						thinned_edges_image[i][j] = 0;
-					}
-				}
-
+				image[i][j] = int(gray.at<uint8_t>(i, j));
 
 			}
+
 		}
 
-
-
-		
-		//Thresholding
-		int ** threshold_image;
-		threshold_image = new int *[out_rows];
-
-		for (int i = 0; i<out_rows; i++) {
-			threshold_image[i] = new int[out_cols];
-			for (int j = 0; j<out_cols; j++) {
-				if (thinned_edges_image[i][j] < 60) {
-					threshold_image[i][j] = 0;
-				}
-				else if (thinned_edges_image[i][j] > 100) {
-					
-					threshold_image[i][j] = 255;
-				}
-				else {
-					threshold_image[i][j] = 127;
-				}
-			}
-		}
-		
-
-		//Hysteresis
-		int ** hysteresis_image;
-		hysteresis_image = new int *[out_rows];
-
-
-		for (int i = 1; i<out_rows; i++) {
-			hysteresis_image[i] = new int[out_cols];
-			for (int j = 1; j<out_cols; j++) {
-				hysteresis_image[i][j] = threshold_image[i][j];
-			}
-		}
-		int image_has_changed = 1;
-		while (image_has_changed == 1) {
-			image_has_changed = 0;
-			for (int i = 1; i<out_rows-1; i++) {
-				for (int j = 1; j<out_cols-1; j++) {
-					if (hysteresis_image[i][j] == 127) {
-						if (hysteresis_image[i - 1][j - 1] == 255 || hysteresis_image[i - 1][j] == 255 || hysteresis_image[i - 1][j + 1] == 255 || hysteresis_image[i][j - 1] == 255 || hysteresis_image[i][j + 1] == 255 || hysteresis_image[i + 1][j - 1] == 255 || hysteresis_image[i + 1][j] == 255 || hysteresis_image[i + 1][j + 1] == 255) {
-							hysteresis_image[i][j] = 255;
-							image_has_changed = 1;
-						}
-					}
-				}
-			}
-		}
-		for (int i = 1; i<out_rows; i++) {
-			for (int j = 1; j<out_cols; j++) {
-				if (hysteresis_image[i][j] == 127) {
-					hysteresis_image[i][j] = 0;
-				}
-			}
-		}
-
-
-
-		//output CV_matrix for display
-		cv::Mat blur_mat(blur_rows, blur_cols, CV_8U);
-
-		//Add pixels to matrix
-		for (int i = 0; i < blur_rows; ++i) {
-			for (int j = 0; j < blur_cols; ++j) {
-				blur_mat.at<uint8_t>(i, j) = uint8_t(blur_image[i][j]);
-				
-
-			}
-		}
-		
-		//Display matrix
-		imshow("Blur_image", blur_mat);
-
-		//___________________
-	
-		//output CV_matrix for display
-		cv::Mat out_mat(out_rows, out_cols, CV_8UC1);
-
-		//Add pixels to matrix
-		for (int i = 0; i < out_rows; ++i) {
-			for (int j = 0; j < out_cols; ++j) {
-				out_mat.at<uint8_t>(i, j) = uint8_t(sobel_image[i][j]);
-				
-				
-			}
-		}
-
-
-		//Display matrix
-		imshow("Sobel_image", out_mat);
-
-		//___________________
-
-	
-
-		//output CV_matrix for display
-		cv::Mat thinned_edges_mat(out_rows, out_cols, CV_8UC1);
-
-		//Add pixels to matrix
-		for (int i = 0; i < out_rows; ++i) {
-			for (int j = 0; j < out_cols; ++j) {
-				thinned_edges_mat.at<uint8_t>(i, j) = uint8_t(thinned_edges_image[i][j]);
-				
-			}
-		}
-
-		//Display matrix
-		imshow("Thinned_edges_image", thinned_edges_mat);
-
-
-		//________________
-
-
-		//output CV_matrix for display
-		cv::Mat threshold_mat(out_rows, out_cols, CV_8UC1);
-
-		//Add pixels to matrix
-		for (int i = 0; i < out_rows; ++i) {
-			for (int j = 0; j < out_cols; ++j) {
-				threshold_mat.at<uint8_t>(i, j) = uint8_t(threshold_image[i][j]);
-
-			}
-		}
-		//Display matrix
-		imshow("Threshold_image", threshold_mat);
-
-
-		//________________
-		
-		//output CV_matrix for display
-		cv::Mat hysteresis_mat(out_rows, out_cols, CV_8UC1);
-
-		//Add pixels to matrix
-		for (int i = 1; i < out_rows-1; ++i) {
-			for (int j = 1; j < out_cols-1; ++j) {
-				hysteresis_mat.at<uint8_t>(i, j) = uint8_t(hysteresis_image[i][j]);
-
-			}
-		}
-		//Display matrix
-		imshow("Hysteresis_image", hysteresis_mat);
-
-
-		//________________
+		edge_detection(image, gray.rows, gray.cols, gaussianFilter, mask_rows, mask_cols, sobelMask_x, sobelMask_y, sobelmask_rows, sobelmask_cols, gaussian_normfactor, false);
 
 
 		if (waitKey(30) >= 0)
 			break;
 
 
-
-		for (int i = 0; i < blur_rows; i++) {
-			delete blur_image[i];
-		}
-		for (int i = 0; i < gray.rows; i++) {
-			delete image[i];
-		}
-		
-		for (int i = 0; i < out_rows; i++) {
-			delete sobel_image[i];
-			delete edges_image[i];
-			delete threshold_image[i];
-			delete thinned_edges_image[i];
-		}
-
-		for (int i = 1; i < out_rows-1; i++) {
-			delete hysteresis_image[i];
-		}
-		
-		delete thinned_edges_image;
-		delete hysteresis_image;
-		delete sobel_image;
-		delete edges_image;
-		delete threshold_image;
-		delete blur_image;
-		delete image;
-		
 	}
-
+	*/
 
 	delete gaussianFilter;
 	delete sobelMask_x, sobelMask_y;
