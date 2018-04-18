6ae307f8830748b4eb61ac561c50be8898ca06ca
Antoine Demeules <antoine.demeules@utt.fr>
2018-04-18 17:50:33 +0800

    added comments to the code

--- Edge_detection/Edge_detection.cpp
+++ Edge_detection/Edge_detection.cpp
@@ -27,17 +27,7 @@ const char* name_of_image;
 
 float PI = 3.14159265;
 
-int** gaussianMask(int std) {	
-	int** result = new int*[3];
-	for (int i = -1; i < 2; i++) {
-		result[i+1] = new int[3];
-		for (int j = -1; j < 2; j++) {
-			result[i+1][j+1] = int(exp(-(float(i*i) + float(j*j)) / (2.0f * float(std))));
-		}
-	}
-	return result;
-}
-
+// Function to save an image in a directory
 void save_image(string outputFileName_str, int **image, int n_rows, int n_cols) {
 	const char* outputFileName = outputFileName_str.c_str();
 
@@ -54,7 +44,7 @@ void save_image(string outputFileName_str, int **image, int n_rows, int n_cols) 
 	
 }
 
-
+// Read the input image (extract pixels value and image informations from the corresponding raw file)
 int ** read_image(const char* image_name) {
 	char buff[5];
 	FILE * imageFile;
@@ -109,10 +99,8 @@ void print3x3Mask(float** mask){
 }
 
 
-
+// Function to compute the canny edge detector on an image
 void edge_detection(int **image, int image_rows, int image_cols, int **gaussianFilter, const int mask_rows, const int mask_cols, int **sobelMask_x, int **sobelMask_y, int sobelmask_rows, int sobelmask_cols, int gaussian_normfactor, bool save) {
-	
-
 	
 	int min = 0;
 	for (int i = 0; i < image_rows; i++) {
@@ -126,30 +114,23 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 	//Output 2d-array
 	int **blur_image;
 
-	//Perform GaussianBlur
+	//Perform GaussianBlur : convolution with the Gaussian mask
 	blur_image = convolution_norm(image, image_rows, image_cols, gaussianFilter, mask_rows, mask_cols, gaussian_normfactor);
 
-
-
-	//Scale our output image
+	//Scale our output image (due to convolution)
 	int blur_rows = image_rows - mask_rows + 1;
 	int blur_cols = image_cols - mask_cols + 1;
 
 
-	
+	int **xmask_image; // x-derivative
+	int **ymask_image; // y derivative
 
-	int **xmask_image;
-	int **ymask_image;
-
-	//Perform Edge detection
-
+	// We use the convolution (with the corresponding Sobel mask) to obtain derivatives
 	xmask_image = convolution(blur_image, blur_rows, blur_cols, sobelMask_x, sobelmask_rows, sobelmask_cols);
 	ymask_image = convolution(blur_image, blur_rows, blur_cols, sobelMask_y, sobelmask_rows, sobelmask_cols);
 
 
-
-
-	//Rescale image
+	//Rescale image (due to convolution)
 	int out_rows = blur_rows - sobelmask_rows + 1;
 	int out_cols = blur_cols - sobelmask_cols + 1;
 
@@ -165,6 +146,7 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 		sobel_image[i] = new int[out_cols];
 		edges_image[i] = new int[out_cols];
 		for (int j = 0; j<out_cols; j++) {
+			// Computation of the magnitude of the gradient
 			sobel_image[i][j] = int(sqrt(xmask_image[i][j] * xmask_image[i][j] + ymask_image[i][j] * ymask_image[i][j]));
 
 
@@ -175,8 +157,9 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 				sobel_image[i][j] = 0;
 			}
 
-
+			// Comutation of the direction of the gradient
 			float temp_dir = atan2(ymask_image[i][j], xmask_image[i][j]) / PI * 180;
+			// The values of the gradient direction are rounded to 4 values : 0 ; 45 ; 90 ; 135
 			if ((temp_dir>-22.5 && temp_dir <= 22.5) || (temp_dir>-157.5 && temp_dir <= 157.5)) {
 				temp_dir = 0;
 			}
@@ -207,22 +190,24 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 
 	for (int i = 1; i<out_rows - 1; i++) {
 		for (int j = 1; j<out_cols - 1; j++) {
-			if (edges_image[i][j] == 0) {
+			// Since the direction of the gradient is perpendicular to the edge, we check the 2 pixels in the gradient's direction
+			// If the current pixel is inferior to on of these 2 pixels then it is set to 0
+			if (edges_image[i][j] == 0) { // gradient direction => vertical 
 				if ((sobel_image[i][j] < sobel_image[i][j + 1]) || (sobel_image[i][j] < sobel_image[i][j - 1])) {
 					thinned_edges_image[i][j] = 0;
 				}
 			}
-			else if (edges_image[i][j] == 90) {
+			else if (edges_image[i][j] == 90) { // gradient direction => horizontal 
 				if ((sobel_image[i][j] < sobel_image[i + 1][j]) || (sobel_image[i][j] < sobel_image[i - 1][j])) {
 					thinned_edges_image[i][j] = 0;
 				}
 			}
-			else if (edges_image[i][j] == 135) {
+			else if (edges_image[i][j] == 135) { // gradient direction => diagonal (top-left => bottom-right)
 				if ((sobel_image[i][j] < sobel_image[i - 1][j + 1]) || (sobel_image[i][j] < sobel_image[i + 1][j - 1])) {
 					thinned_edges_image[i][j] = 0;
 				}
 			}
-			else if (edges_image[i][j] == 45) {
+			else if (edges_image[i][j] == 45) { // gradient direction => diagonal (bot-left => top-right)
 				if ((sobel_image[i][j] < sobel_image[i + 1][j + 1]) || (sobel_image[i][j] < sobel_image[i - 1][j - 1])) {
 					thinned_edges_image[i][j] = 0;
 				}
@@ -243,13 +228,15 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 		threshold_image[i] = new int[out_cols];
 		for (int j = 0; j<out_cols; j++) {
 			if (thinned_edges_image[i][j] < 100) {
+				// We consider that the value is too low and this pixel doesn't belong to an edge
 				threshold_image[i][j] = 0;
 			}
 			else if (thinned_edges_image[i][j] > 200) {
-
+				// We consider that the value is high enought so this pixel belong to an edge
 				threshold_image[i][j] = 255;
 			}
 			else {
+				// We can't say if this pixel belongs or not to an edge (requires additional processing: hysteresis)
 				threshold_image[i][j] = 127;
 			}
 		}
@@ -257,9 +244,10 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 
 
 	//Hysteresis
+
+	// Copying the previous file into a new one
 	int ** hysteresis_image;
 	hysteresis_image = new int *[out_rows];
-
 
 	for (int i = 0; i<out_rows; i++) {
 		hysteresis_image[i] = new int[out_cols];
@@ -267,12 +255,18 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 			hysteresis_image[i][j] = threshold_image[i][j];
 		}
 	}
-	int image_has_changed = 1;
+	
+	
+	// This loop continues until no pixel is changed (all pixels belonging to an edge have been detected).
+	// With each iteration, we apply hysteresis to all the pixels of the image (top-left to bottom-right)
+	int image_has_changed = 1; //value used as a boolean
 	while (image_has_changed == 1) {
 		image_has_changed = 0;
 		for (int i = 1; i<out_rows - 1; i++) {
 			for (int j = 1; j<out_cols - 1; j++) {
 				if (hysteresis_image[i][j] == 127) {
+					// Checking if one of the 8 neigboring pixels has his value equal to 255 (belong to an edge)
+					// If there is at least one then the current pixel also belong to the edge (his value is set to 255)
 					if (hysteresis_image[i - 1][j - 1] == 255 || hysteresis_image[i - 1][j] == 255 || hysteresis_image[i - 1][j + 1] == 255 || hysteresis_image[i][j - 1] == 255 || hysteresis_image[i][j + 1] == 255 || hysteresis_image[i + 1][j - 1] == 255 || hysteresis_image[i + 1][j] == 255 || hysteresis_image[i + 1][j + 1] == 255) {
 						hysteresis_image[i][j] = 255;
 						image_has_changed = 1;
@@ -281,6 +275,7 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 			}
 		}
 	}
+	// All unclassified pixels (value = 127) are set to 0
 	for (int i = 1; i<out_rows; i++) {
 		for (int j = 1; j<out_cols; j++) {
 			if (hysteresis_image[i][j] == 127) {
@@ -289,11 +284,6 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 		}
 	}
 
-
-	
-
-
-	
 	//output CV_matrix for display
 	cv::Mat blur_mat(blur_rows, blur_cols, CV_8U);
 
@@ -301,8 +291,6 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 	for (int i = 0; i < blur_rows; ++i) {
 		for (int j = 0; j < blur_cols; ++j) {
 			blur_mat.at<uint8_t>(i, j) = uint8_t(blur_image[i][j]);
-
-
 		}
 	}
 
@@ -323,13 +311,10 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 		}
 	}
 
-
 	//Display matrix
 	imshow("Sobel_image", out_mat);
 
 	//___________________
-
-
 
 	//output CV_matrix for display
 	cv::Mat thinned_edges_mat(out_rows, out_cols, CV_8UC1);
@@ -379,10 +364,7 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 	imshow("Hysteresis_image", hysteresis_mat);
 
 
-	//________________
-	
-
-	
+	//________________	
 	
 	//Saves the images
 	if(save == true){
@@ -428,18 +410,10 @@ void edge_detection(int **image, int image_rows, int image_cols, int **gaussianF
 
 int main() {
 
-	//Open Video stream
-	/*
-	VideoCapture stream1(1);   //0 is the id of video device.0 if you have only one camera.
-	if (!stream1.isOpened()) { //check if video device has been initialisedc
-		cout << "cannot open camera";
-	}
-	*/
+	/* INITIALIZATION OF THE DIFFERENT MASKS */
 
-
-	// The user can choose sigma
+	// The user can choose sigma (float between 0 and 3)
 	bool run2 = false;
-	//Edgedetection 
 	float sigma;
 	while (!run2) {
 		cout << "Please choose the value of sigma (float between 0 and 3)" << endl;
@@ -452,9 +426,10 @@ int main() {
 		}
 	}
 
+	// Generate the corresponding Gaussian mask (width >= 5 sigma)
 	int width = 1;
 	while ((width / (5 * sigma)) < 1) {
-		width = width + 2;
+		width = width + 2; // increase the width (only odd values)
 	}
 	cout << "The size of the gaussian mask is " << width << endl;
 	const int mask_rows = width;
@@ -470,15 +445,13 @@ int main() {
 			gaussianFilter_float[i + (width / 2)][j + (width / 2)] = (1.0/(2.0*PI*sigma* sigma))*exp(-(float(i*i) + float(j*j)) / (2.0f * sigma* sigma));
 			if (gaussianFilter_float[i + (width / 2)][j + (width / 2)] < smallest_gaussian && gaussianFilter_float[i + (width / 2)][j + (width / 2)] > 0.001) {
 				smallest_gaussian = gaussianFilter_float[i + (width / 2)][j + (width / 2)];
-			}
-			
-		}
-		
+			}			
+		}		
 	}
 	
-	cout << "Smallest Gaussian :  " << smallest_gaussian << width << endl;
+	// cout << "Smallest Gaussian :  " << smallest_gaussian << width << endl;
 
-	
+	// Normalization of the Gaussian mask
 	int gaussian_normfactor = 0;
 	int** gaussianFilter = new int*[mask_rows];
 	cout << "[";
@@ -495,15 +468,12 @@ int main() {
 	cout << "]" << endl;
 
 
-	//Edge detection:
-	//Dimentions of filter mask
-
+	// SOBEL FILTER MASK
 	const int sobelmask_rows = 3;
 	const int sobelmask_cols = 3;
 
 	int **sobelMask_y;
 	sobelMask_y = new int *[sobelmask_rows];
-
 
 	int **sobelMask_x;
 	sobelMask_x = new int *[sobelmask_rows];
@@ -531,13 +501,14 @@ int main() {
 			sobelMask_y[i][j] = tempy[i][j];
 		}
 	}
-	
+	// end of masks' initialization
 
-	bool run = true;
-	//Edgedetection 
+	// CHOICE OF THE INPUT IMAGE
+	bool run = true; 
 	while (run) {
 		int **image;
 
+		// The user can choose the input image in a list
 		int which_image = 0;
 		cout << "Please input a number between 0 and 5 to chose image, (0 => leaf ; 1 => cana ; 2 => fruit ; 3 => img335 ; 4 => lamp), or 5 for exit" << endl;
 		cin >> which_image;
@@ -573,6 +544,7 @@ int main() {
 		cout << "Press Q to go back to main menu" << endl;
 
 		if(run==true){
+			//EDGE DETECTION
 			edge_detection(image, rows, cols, gaussianFilter, mask_rows, mask_cols, sobelMask_x, sobelMask_y, sobelmask_rows, sobelmask_cols, gaussian_normfactor, true);
 			while(true){
 				if (waitKey(30) >= 0)
@@ -581,8 +553,6 @@ int main() {
 		
 		}
 	}	
-	
-	
 
 	delete gaussianFilter;
 	delete sobelMask_x, sobelMask_y;
